83c9470c03097775d69799a16e14fd7c9a956b80
Sara Ribas <sararibas@Saras-iMac.local>
2025-12-18 15:53:06 +0000

    style: update button and focus colors plus alert margin

--- src/components/alert/Alert.tsx
+++ src/components/alert/Alert.tsx
@@ -8,7 +8,7 @@ interface AlertProps {
 const Alert = ({ type, children }: AlertProps) => {
   return (
     <div
-      className={`alert mb-0 fade show position-fixed top-0 start-50 translate-middle-x mt-3 alert-${type} $type ?? ""`}
+      className={`alert mb-0 fade show position-fixed top-0 start-50 translate-middle-x mt-5 alert-${type} $type ?? ""`}
       role="alert"
     >
       {children}
